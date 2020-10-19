8e0b0aaf7862f4400b69c6dc4e0056dec401b40e
Luteranya Nyange <dieume.dev@gmail.com>
2020-10-19 13:29:08 +0300

    database and migrations support with sqlalchemy

--- yumroad/__init__.py
+++ yumroad/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 
 from yumroad.config import configurations
+from yumroad.extensions import db, migrate
 
 
 def create_app(environment_name="dev"):
 
     app = Flask(__name__)
     app.config.from_object(configurations[environment_name])
+
+    # Initialize extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     return app
--- yumroad/config.py
+++ yumroad/config.py
@@ -4,14 +4,18 @@ import os
 class BaseConfig:
     TESTING = False
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = "mysql://root:pwd@localhost:3306"
 
 
 class DevConfig(BaseConfig):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:/// dev.db"
+    SQLALCHEMY_ECHO = True
 
 
 class TestConfig(BaseConfig):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:/// test.db"
 
 
 class ProdConfig(BaseConfig):
