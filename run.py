a523711131e74a844ecbe81b77584c58e0962340
Luteranya Nyange <dieume.dev@gmail.com>
2020-10-19 13:16:24 +0300

    basic configuration

---
+++ run.py
@@ -0,0 +1,3 @@
+from yumroad import create_app
+
+app = create_app()
--- yumroad/__init__.py
+++ yumroad/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
+from yumroad.config import configurations
 
-def create_app():
+
+def create_app(environment_name="dev"):
 
     app = Flask(__name__)
-
+    app.config.from_object(configurations[environment_name])
 
     return app
