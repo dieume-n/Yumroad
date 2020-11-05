e8e5af27908d9c40e05bf240b1c7771cd1956814
Luteranya Nyange <dieume.dev@gmail.com>
2020-11-05 12:15:47 +0300

    redirect unauthenticated user to the login page

--- yumroad/users/views.py
+++ yumroad/users/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for, render_template, redirect, flash
+from flask import Blueprint, url_for, render_template, redirect, flash, session, request
 from flask_login import current_user, login_user, logout_user, login_required
 
 from yumroad.extensions import login_manager
@@ -11,6 +11,12 @@ users_bp = Blueprint("users", __name__)
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    session["after_login"] = request.url
+    return redirect(url_for("users.login"))
 
 
 @users_bp.route("/register", methods=["GET", "POST"])
@@ -34,14 +40,15 @@ def login():
 
         if user and user.check_password(form.password.data):
             login_user(user)
-            return redirect(url_for("products.index"))
+            flash(f"Welcome {user.name}", "success")
+            return redirect(session.get("after_login") or url_for("products.index"))
         else:
             flash("Invalid email or password", category="danger")
     return render_template("users/login.html", form=form, title="Login")
 
 
-@login_required
 @users_bp.route("/logout")
+@login_required
 def logout():
     logout_user()
     return redirect(url_for("products.index"))
