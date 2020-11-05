295aa6bf6d1c55ee02b6e4d01aa6a4c2c25165ca
Luteranya Nyange <dieume.dev@gmail.com>
2020-11-05 14:57:38 +0300

    a better unauthorized handling of requests

--- yumroad/users/views.py
+++ yumroad/users/views.py
@@ -2,10 +2,14 @@ from flask import Blueprint, url_for, render_template, redirect, flash, session,
 from flask_login import current_user, login_user, logout_user, login_required
 
 from yumroad.extensions import login_manager
+from yumroad.helpers import is_safe_url
 from yumroad.users.models import User
 from yumroad.users.forms import RegisterForm, LoginForm
 
 users_bp = Blueprint("users", __name__)
+
+login_manager.login_view = "users.login"
+login_manager.login_message_category = "info"
 
 
 @login_manager.user_loader
@@ -13,27 +17,33 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-@login_manager.unauthorized_handler
-def unauthorized():
-    session["after_login"] = request.url
-    flash("You need to login", "warning")
-    return redirect(url_for("users.login"))
-
-
 @users_bp.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        flash("You are already logged in", "info")
+        return redirect(url_for("product.index"))
+
     form = RegisterForm()
     if form.validate_on_submit():
         user = User(form.name.data, form.email.data, form.password.data)
         user.save_to_db()
-        return redirect(url_for("products.index"))
+
+        login_user(user)
+        flash("Registration successful", "success")
+        next = request.args.get("next")
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return redirect(next or url_for("products.index"))
+        # return redirect(session.get("after_login") or url_for("products.index"))
     return render_template("users/register.html", form=form, title="Resgister")
 
 
 @users_bp.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("products.index"))
+        flash("You are already logged in", "info")
+        return redirect(url_for("product.index"))
 
     form = LoginForm()
     if form.validate_on_submit():
@@ -42,7 +52,11 @@ def login():
         if user and user.check_password(form.password.data):
             login_user(user)
             flash(f"Welcome {user.name}", "success")
-            return redirect(session.get("after_login") or url_for("products.index"))
+            next = request.args.get("next")
+            if not is_safe_url(next):
+                return flask.abort(400)
+
+            return redirect(next or url_for("products.index"))
         else:
             flash("Invalid email or password", category="danger")
     return render_template("users/login.html", form=form, title="Login")
