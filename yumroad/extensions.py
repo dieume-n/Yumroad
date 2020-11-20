5765ba077de3b3ae88d8266afcdfebb252700d7c
Luteranya Nyange <dieume.dev@gmail.com>
2020-11-20 18:05:10 +0300

    uniform naming convention

--- yumroad/extensions.py
+++ yumroad/extensions.py
@@ -3,10 +3,20 @@ from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
+from sqlalchemy import MetaData
 
 from faker import Faker
 
-db = SQLAlchemy()
+
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 fake = Faker("en_US")
 csrf = CSRFProtect()
